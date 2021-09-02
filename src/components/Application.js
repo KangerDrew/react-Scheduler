32a73c93c051c7af5779931471f33de78935c796
KangerDrew <kangdrew95@gmail.com>
2021-09-02 16:09:40 -0700

    Installed axios package, and replaced dummy data with the psql data from vagrant

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from './DayList';
 import Appointment from "components/Appointment/index";
 
 // Dummy data for displays
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -74,6 +75,14 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(()=> {
+    const linkDays = 'http://localhost:8001/api/days';
+    axios.get(linkDays)
+      .then(response => setDays([...response.data]));
+  },[])
+
   const appointmentArr = appointments.map(appointment => {
     return <Appointment key={appointment.id} {...appointment} />
   })
