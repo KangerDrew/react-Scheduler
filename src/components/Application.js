19f87324b39ab624534fd77658b523865c8148d7
KangerDrew <kangdrew95@gmail.com>
2021-09-02 20:01:05 -0700

    Combined all states into one. Fixed dependency issue w/ useEffect also.

--- src/components/Application.js
+++ src/components/Application.js
@@ -74,8 +74,16 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days}));
+
 
   useEffect(()=> {
     const linkDays = 'http://localhost:8001/api/days';
@@ -98,8 +106,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
