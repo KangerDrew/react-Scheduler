0e8f939d88bfcc32b4243a2d3dc0cd355bfcc533
KangerDrew <kangdrew95@gmail.com>
2021-09-03 13:05:48 -0700

    Minor change to API fetch line

--- src/components/Application.js
+++ src/components/Application.js
@@ -93,7 +93,9 @@ export default function Application(props) {
       axios.get(linkAppointments)
     ])
       .then((all) => {
-        setState(prev => ({...prev, days:all[0].data, appointments:all[1].data }))
+        const days = all[0].data;
+        const appointments = all[1].data;
+        setState(prev => ({...prev, days, appointments}))
       })
     
   },[])
